6b6cbd1c4deb7126725b4bf19d3a2f86c7712b83
Gregory Shtrasberg <Gregory.Shtrasberg@amd.com>
2024-08-27 19:31:33 +0000

    IInteractive docker warpper

--- docker.sh
+++ docker.sh
@@ -1,2 +1,63 @@
 #!/bin/bash
-docker run -it --rm --device=/dev/kfd --device=/dev/dri --mount type=bind,source=/home/gshtrasb/Projects,target=/projects --mount type=bind,source=/data/models,target=/models --group-add video --cap-add=SYS_PTRACE $@
+#set -x
+dry_run=0
+interactive=0
+grep_value=
+
+for i in "$@"; do
+  case $i in
+  -n|--name)
+    name="$2"
+    shift
+  ;;
+  -d|--dry=run)
+    dry_run=1
+  ;;
+  -g|--grep)
+    grep_value="$2"
+    shift
+  ;;
+  -i|--interactive)
+    interactive=1
+  ;;
+  *)
+    image=$1
+  ;;
+  esac
+  shift
+done
+
+if [[ $interactive == 1 ]] ; then
+    grep_arg=
+    if [[ $grep_value != "" ]] ; then
+      grep_arg=" | grep $grep_value"
+    fi
+    cmd="docker images | awk '{print \$1 \":\" \$2}' | grep -v none | grep -v REPOSITORY:TAG $grep_arg"
+    images=$(eval $cmd)
+    i=0
+    for im in $images ; do
+    echo "$i $im"
+    i=$((i+1))
+    done
+    read -p "Select image: " selection
+    images_arr=($images)
+    image=${images_arr[$selection]}
+fi
+echo "Image: $image"
+
+if [[ $name == "" ]] ; then
+    read -p "Container name: " name
+fi
+
+name_arg=
+if [[ $name != "" ]] ; then
+    name_arg=" --name $name"
+fi
+
+if [[ $dry_run != 1 ]] ; then
+tmux rename-window "Docker:$name"
+docker run -it --rm --device=/dev/kfd --device=/dev/dri --mount type=bind,source=/home/gshtrasb/Projects,target=/projects --mount type=bind,source=/data/models,target=/models --group-add video --cap-add=SYS_PTRACE $name_arg $image
+tmux rename-window "zsh"
+else
+echo "docker run -it --rm --device=/dev/kfd --device=/dev/dri --mount type=bind,source=/home/gshtrasb/Projects,target=/projects --mount type=bind,source=/data/models,target=/models --group-add video --cap-add=SYS_PTRACE $name_arg $image"
+fi
