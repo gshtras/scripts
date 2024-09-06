a8bb9ad3cc842f9c186f919a81d725460bc07626
Gregory Shtrasberg <Gregory.Shtrasberg@amd.com>
2024-09-06 20:08:23 +0000

    hipblaslt build

---
+++ build_hipblaslt.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+git clone https://github.com/ROCm/hipBLAS-common.git
+cd hipBLAS-common
+mkdir build
+cd build
+cmake ..
+make package install
+cd ../..
+
+git clone https://github.com/ROCm/hipBLASLt
+cd hipBLASLt
+SCCACHE_IDLE_TIMEOUT=1800 ./install.sh -idc --architecture gfx942
