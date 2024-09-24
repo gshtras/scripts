af23424aded3605869998d3f83a5af3a6c392b5e
Gregory Shtrasberg <Gregory.Shtrasberg@amd.com>
2024-09-24 19:55:32 +0000

    sort uniq

--- docker_top.sh
+++ docker_top.sh
@@ -9,13 +9,15 @@ for pid in $pids ; do
 
     new_cids=$(for cid in $(docker ps | grep -v CONTAINER | awk '{print $1}') ; do docker top $cid | grep $pid > /dev/null && echo $cid ; done)
     for new_cid in $new_cids ; do
-        echo $new_cid
-        cids+=( $new_cid)
+        echo "Container: $new_cid"
+        cids+=( $new_cid )
     done
 done
 echo "Result"
 
-for cid in ${cids[@]} ; do
+sorted_unique_cids=($(echo "${cids[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
+
+for cid in ${sorted_unique_cids[@]} ; do
     echo "Container ID: ${cid}"
     docker inspect $cid | grep home | head -1
 done
