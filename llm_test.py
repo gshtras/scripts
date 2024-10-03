9743a93ebad43d70af823a7de2de742474b08aef
Gregory Shtrasberg <Gregory.Shtrasberg@amd.com>
2024-10-03 16:58:39 -0500

    rpd and more args

--- llm_test.py
+++ llm_test.py
@@ -1,45 +1,48 @@
-from typing import Any
-from simple_term_menu import TerminalMenu
 import argparse
+import os
+import sqlite3
 import time
+from contextlib import contextmanager, nullcontext
+from typing import Any
 
+import vllm.envs as envs
+from simple_term_menu import TerminalMenu
 from vllm import LLM, SamplingParams
+from vllm.inputs.data import TokensPrompt
 
 
 class LlmKwargs(dict):
 
-    def __init__(self):
-        self.kwargs = {}
-        self.batch_size = 1
-        self.max_tokens = 256
-        self.prompt = "There is a round table in the middle of the"
-
-    def from_args(self, args: argparse.Namespace):
-        self.kwargs['model'] = args.model
-        self.kwargs['kv_cache_dtype'] = args.kv_cache_dtype
-        self.kwargs['tensor_parallel_size'] = args.tensor_parallel_size
-        self.kwargs['dtype'] = args.dtype
-        self.kwargs['quantization'] = args.quantization
-        self.kwargs['enforce_eager'] = args.enforce_eager
-        self.prompt = args.prompt
+    def __init__(self, args: argparse.Namespace) -> None:
+        self.kwargs = {
+            'model': args.model,
+            'kv_cache_dtype': args.kv_cache_dtype,
+            'tensor_parallel_size': args.tensor_parallel_size,
+            'dtype': args.dtype,
+            'quantization': args.quantization,
+            'enforce_eager': args.enforce_eager,
+        }
+        self.prompt = args.prompt if args.input_len == -1 else [
+            0
+        ] * args.input_len
         self.batch_size = args.batch_size
-        self.max_tokens = args.max_tokens
+        self.sampling_params = SamplingParams(temperature=args.temperature,
+                                              top_p=1,
+                                              max_tokens=args.max_tokens,
+                                              ignore_eos=args.ignore_eos)
+        self.prompt = "There is a round table in the middle of the"
 
     def __setitem__(self, key: str, value: str) -> None:
         self.kwargs[key] = value
-
-    def print(self):
-        for key, value in self.kwargs.items():
-            print(f"{key}: {value}")
 
     def __str__(self) -> str:
         res = "===================\n"
         for key, value in self.kwargs.items():
             res += f"{key}: {value}\n"
-        res += "\n Sampling params: \n"
+        res += f"Sampling params: {self.sampling_params}\n"
+        res += "\n Misc: \n"
         res += f"Prompt: {self.prompt}\n"
         res += f"Batch size: {self.batch_size}\n"
-        res += f"Max tokens: {self.max_tokens}\n"
         return res
 
 
@@ -65,8 +68,22 @@ def select_prompt(llm_kwargs: LlmKwargs):
 def select_batch_size(llm_kwargs: LlmKwargs):
     llm_kwargs.batch_size = int(input("Enter a batch size: "))
 
+
 def select_max_tokens(llm_kwargs: LlmKwargs):
-    llm_kwargs.max_tokens = int(input("Enter max tokens: "))
+    llm_kwargs.sampling_params.max_tokens = int(input("Enter max tokens: "))
+
+
+def select_input_len(llm_kwargs: LlmKwargs):
+    llm_kwargs.prompt = [0] * int(input("Enter input length: "))
+
+
+def select_temperature(llm_kwargs: LlmKwargs):
+    llm_kwargs.sampling_params.temperature = float(
+        input("Enter temperature: "))
+
+
+def select_ignore_eos(llm_kwargs: LlmKwargs):
+    llm_kwargs.sampling_params.ignore_eos = [False, True][menu([False, True])]
 
 
 values = {
@@ -79,6 +96,9 @@ values = {
     "prompt": select_prompt,
     "batch_size": select_batch_size,
     "max_tokens": select_max_tokens,
+    "input_len": select_input_len,
+    "temperature": select_temperature,
+    "ignore_eos": select_ignore_eos,
     "Done": None
 }
 
@@ -87,7 +107,7 @@ def menu(items):
     terminal_menu = TerminalMenu([str(x) for x in items])
     menu_entry_index = terminal_menu.show()
     if menu_entry_index is None:
-        print("Abroted")
+        print("Aborted")
         exit(1)
     return menu_entry_index
 
@@ -109,19 +129,49 @@ def interactive(llm_kwargs: LlmKwargs):
         print(llm_kwargs)
 
 
+def recreate_trace(args: argparse.Namespace):
+    from rocpd.schema import RocpdSchema
+    if envs.VLLM_RPD_PROFILER_DIR is None:
+        envs.VLLM_RPD_PROFILER_DIR = os.path.join(os.path.curdir, "trace.rpd")
+    try:
+        os.remove(envs.VLLM_RPD_PROFILER_DIR)
+    except FileNotFoundError:
+        pass
+    schema = RocpdSchema()
+    connection = sqlite3.connect(envs.VLLM_RPD_PROFILER_DIR)
+    schema.writeSchema(connection)
+    connection.commit()
+
+
 def main(args: argparse.Namespace):
-    llm_args = LlmKwargs()
-    llm_args.from_args(args)
+
+    @contextmanager
+    def rpd_profiler_context():
+        from rpdTracerControl import rpdTracerControl as rpd
+        llm.start_profile()
+        yield
+        llm.stop_profile()
+        rpd.top_totals()
+
+    llm_args = LlmKwargs(args)
     print(llm_args)
     if args.interactive:
         interactive(llm_args)
 
     batch_size = llm_args.batch_size
+
+    if args.rpd:
+        recreate_trace(args)
+
     llm = LLM(**llm_args.kwargs)
-    sampling_params = SamplingParams(temperature=0, top_p=1, max_tokens=llm_args.max_tokens)
+
     start_time = time.perf_counter()
-    outs = llm.generate([llm_args.prompt] * batch_size,
-                        sampling_params=sampling_params)
+    with rpd_profiler_context() if args.rpd else nullcontext():
+        outs = llm.generate([TokensPrompt(prompt_token_ids=llm_args.prompt)] *
+                            llm_args.batch_size if isinstance(
+                                llm_args.prompt, list) else [llm_args.prompt] *
+                            batch_size,
+                            sampling_params=llm_args.sampling_params)
     end_time = time.perf_counter()
     elapsed_time = end_time - start_time
 
@@ -165,10 +215,16 @@ if __name__ == "__main__":
                         type=str,
                         default=None,
                         choices=values["quantization"])
-    parser.add_argument('--prompt', type=str, default="There is a round table in the middle of the")
+    parser.add_argument('--prompt',
+                        type=str,
+                        default="There is a round table in the middle of the")
     parser.add_argument('--batch-size', type=int, default=1)
     parser.add_argument('--max-tokens', type=int, default=256)
     parser.add_argument('--enforce-eager', action='store_true')
+    parser.add_argument('--rpd', action='store_true')
+    parser.add_argument('--input-len', type=int, default=-1)
+    parser.add_argument('--temperature', type=float, default=0)
+    parser.add_argument('--ignore-eos', action='store_true')
     args = parser.parse_args()
 
     main(args)
