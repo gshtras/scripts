3dab11dffd62c4c4c424611ae9cde892e2dae7b9
Gregory Shtrasberg <Gregory.Shtrasberg@amd.com>
2024-11-04 15:12:51 -0600

    More checks and less noise

--- regression_test/run.sh
+++ regression_test/run.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-cd $SCRIPT_DIR
+set -e
 
-../docker.sh -n regression -c "bash /projects/scripts/regression_test/rebuild_and_run.sh"
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+cd $SCRIPT_DIR/../docker
+$SCRIPT_DIR/../docker/create.sh
+cd -
+$SCRIPT_DIR/../docker.sh -n regression -c "bash /projects/scripts/regression_test/rebuild_and_run.sh"
