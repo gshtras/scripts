10843971f1e24242f57150d35825eea90e37a262
Gregory Shtrasberg <Gregory.Shtrasberg@amd.com>
2024-11-18 13:47:39 -0600

    No need to build gradlib separately anymore

--- regression_test/rebuild_and_run.sh
+++ regression_test/rebuild_and_run.sh
@@ -10,6 +10,6 @@ git clone https://github.com/rocm/vllm
 cd vllm
 git checkout develop
 pip install -r requirements-rocm.txt
-$SCRIPT_DIR/../rebuild.sh --gradlib |& tee /projects/build_regression.log
+$SCRIPT_DIR/../rebuild.sh |& tee /projects/build_regression.log
 $SCRIPT_DIR/run_vllm.sh |& tee /projects/result_regression.log
 python $SCRIPT_DIR/process.py
