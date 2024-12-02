a6fa21337b09f5743c6d15d0f06fa3cc19b5eccd
Gregory Shtrasberg <Gregory.Shtrasberg@amd.com>
2024-12-02 13:43:15 -0600

    p3l for mixtral; reenabled custom fa for vision

--- regression_test/run_vllm.sh
+++ regression_test/run_vllm.sh
@@ -20,7 +20,7 @@ function run_perf()
     if [[ $out != "1" ]] ; then
         misc="--num-scheduler-steps 10"
     fi
-    log_name=/projects/tmp/$(echo "${model}" | sed -e 's/\//_/g')_${batch}_${in}_${out}_${tp}.log
+    log_name=/projects/tmp/$(echo "${model}" | sed -e 's/\//_/g')_${batch}_${in}_${out}_${tp}_${dtype}.log
     python benchmarks/benchmark_latency.py --enable-chunked-prefill False --load-format dummy --dtype $dtype --num-iters-warmup 2 --num-iters 5 --batch-size $batch $misc --input-len $in --output-len $out --model /models/$model -tp $tp $eager $@ &> $log_name
     echo "${model},${batch},${in},${out},${tp},${dtype},$(cat $log_name | grep "Avg latency:" | awk '{print $3}')"
     set -e
@@ -45,7 +45,7 @@ function run_vision()
     shift
     echo $model
     log_name=/projects/tmp/vision_$(echo "${model}" | sed -e 's/\//_/g').log
-    VLLM_USE_TRITON_FLASH_ATTN=0 python /projects/llm_test.py --model /models/$model --image-path /projects/image1.jpg --prompt "Describe this image" -tp 4 $@ &> $log_name
+    python /projects/llm_test.py --model /models/$model --image-path /projects/image1.jpg --prompt "Describe this image" -tp 4 $@ &> $log_name
     grep "Generated:" $log_name
     set -e
 }
@@ -122,5 +122,6 @@ run_perf Meta-Llama-3.1-70B-Instruct-FP8-KV 16 1024 1024 8 float16
 
 echo "===P3L==="
 
-run_p3l Meta-Llama-3.1-8B-Instruct 1024 512 10 --dtype float16
-run_p3l Meta-Llama-3.1-70B-Instruct-FP8-KV 1024 512 10 --kv-cache-dtype fp8 --dtype float16
+run_p3l Meta-Llama-3.1-8B-Instruct 1024 512 15 --dtype float16
+run_p3l Meta-Llama-3.1-70B-Instruct-FP8-KV 1024 512 15 --kv-cache-dtype fp8 --dtype float16
+run_p3l mistral-ai-models/Mixtral-8x22B-v0.1/ 1024 512 15 -tp 4 --dtype float16
