46c830694c139c4ec8c6aa94cb3484b9e71fccbc
Gregory Shtrasberg <Gregory.Shtrasberg@amd.com>
2024-12-03 09:52:44 -0600

    Fix permissions and syntax

--- docker.sh
+++ docker.sh
@@ -22,7 +22,7 @@ while [[ $# -gt 0 ]] ; do
     name="$2"
     shift
   ;;
-  --dry=run)
+  --dry-run)
     dry_run=1
   ;;
   -g|--grep)
@@ -76,9 +76,9 @@ fi
 
 if [[ $dry_run != 1 ]] ; then
 tmux rename-window "Docker:$name"
-docker run ${it} --rm --device=/dev/kfd --device=/dev/dri --mount type=bind,source=/home/gshtrasb/Projects,target=/projects --mount type=bind,source=/data/models,target=/models --ulimit core=0:0 --ulimit memlock -1:-1 $extra_args --group-add video --cap-add=SYS_PTRACE $name_arg $image $command
+docker run ${it} --rm --device=/dev/kfd --device=/dev/dri --mount type=bind,source=/home/gshtrasb/Projects,target=/projects --mount type=bind,source=/data/models,target=/models --ulimit core=0:0 --ulimit memlock=-1:-1 $extra_args --group-add video --cap-add=SYS_PTRACE $name_arg $image $command
 tmux setw automatic-rename on
 echo "Finished docker image $image"
 else
-echo "docker run ${it} --rm --device=/dev/kfd --device=/dev/dri --mount type=bind,source=/home/gshtrasb/Projects,target=/projects --mount type=bind,source=/data/models,target=/models --ulimit core=0:0 --ulimit memlock -1:-1 $extra_args --group-add video --cap-add=SYS_PTRACE $name_arg $image $command"
+echo "docker run ${it} --rm --device=/dev/kfd --device=/dev/dri --mount type=bind,source=/home/gshtrasb/Projects,target=/projects --mount type=bind,source=/data/models,target=/models --ulimit core=0:0 --ulimit memlock=-1:-1 $extra_args --group-add video --cap-add=SYS_PTRACE $name_arg $image $command"
 fi
