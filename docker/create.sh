e7b7d138d8b26403946668b2fac2a414e2ac1ddf
Gregory Shtrasberg <Gregory.Shtrasberg@amd.com>
2025-01-13 10:13:29 -0600

    More docker creation customization

--- docker/create.sh
+++ docker/create.sh
@@ -2,6 +2,28 @@
 set -e
 set -x
 
+name=${USER}_vllm
+base=rocm/vllm-dev:base
+
+while [[ $# -gt 0 ]] ; do
+  i=$1
+  case $i in
+  -n|--name)
+    name="$2"
+    shift
+  ;;
+  -b|--base)
+    base="$2"
+    shift
+  ;;
+  *)
+    echo "Unrecognized argument: $1"
+    exit 1
+  ;;
+  esac
+  shift
+done
+
 rm -f requirements-lint.txt* bundle.tgz
 
 wget 'https://raw.githubusercontent.com/ROCm/vllm/refs/heads/main/requirements-lint.txt'
@@ -16,11 +38,12 @@ USER=${USER:-$(whoami)}
 PYTORCH_ROCM_ARCH=${PYTORCH_ROCM_ARCH:-$(/opt/rocm/bin/offload-arch)}
 
 docker build -f Dockerfile.vllm \
+    --build-arg "BASE_IMAGE=${base}" \
     --build-arg "UID=$(id -u)" \
     --build-arg "GID=$(id -g)" \
     --build-arg "USERNAME=$(whoami)" \
     --build-arg "RENDER_GID=$(cat /etc/group | grep render | cut -d: -f3)" \
     --build-arg "PYTORCH_ROCM_ARCH=${PYTORCH_ROCM_ARCH}" \
-    -t ${USER}_vllm .
+    -t ${name} .
 
 rm -f requirements-lint.txt bundle.tgz
